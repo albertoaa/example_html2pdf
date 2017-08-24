4f24f7568b6491d58f692bc37d32311175ea2c15
Alberto Aragon Alvarez <ronin8503@gmail.com>
2017-08-23 22:07:56 -0500

    Adding external content and form to generate pdf

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
     require './vendor/autoload.php';
     use Spipu\Html2Pdf\Html2Pdf;
 
-    $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
-    $html2pdf->writeHTML('<h1>HelloWorld</h1><h2>Más información</h2><p>This is my first test</p>');
-    $html2pdf->output('generated_document.pdf');
+    if (isset($_POST['create'])){
+        ob_start();
+        require_once 'print_view.php';
+        $html = ob_get_clean();
+
+        $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+        $html2pdf->writeHTML($html);
+        $html2pdf->output('generated_document.pdf');
+    }
+?>
+
+<form action="" method="post">
+    <input type="submit" value="Create PDF" name="create"/>
+</form>
