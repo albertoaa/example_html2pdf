29192f45f007e2f3f2e72381d7df8c08bce6fd24
Alberto Aragon Alvarez <ronin8503@gmail.com>
2017-08-23 21:50:18 -0500

    Adding some options

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     require './vendor/autoload.php';
     use Spipu\Html2Pdf\Html2Pdf;
 
-    $html2pdf = new Html2Pdf();
-    $html2pdf->writeHTML('<h1>HelloWorld</h1><p>This is my first test</p>');
-    $html2pdf->output();
+    $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+    $html2pdf->writeHTML('<h1>HelloWorld</h1><h2>Más información</h2><p>This is my first test</p>');
+    $html2pdf->output('generated_document.pdf');
