1f5de2a274d6a01ef3595c120f746539d7f1dc7c
Alberto Aragon Alvarez <ronin8503@gmail.com>
2017-08-24 23:42:32 -0500

    Adding more elements and styles

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
         require_once 'print_view.php';
         $html = ob_get_clean();
 
-        $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+        $html2pdf = new Html2Pdf('P','A4','en','true','UTF-8');
         $html2pdf->writeHTML($html);
         $html2pdf->output('generated_document.pdf');
     }
 ?>
 
-<form action="" method="post">
+<form action="" method="post" style="text-align: center">
+    <img src="https://www.html2pdf.fr/img/_langue/en/logo.gif" alt="HTML2pdf" style="margin-bottom: 10px;"/><br>
     <input type="text" placeholder="Title" name="title">
     <input type="submit" value="Create PDF" name="create"/>
 </form>
--- print_view.php
+++ print_view.php
@@ -14,15 +14,68 @@
         h2 {
             color: blue;
         }
+        #boxes {
+            width: 100%;
+        }
+        .box {
+            width: 150px;
+            height: 150px;
+            margin-left: 10px;
+            border: 1px solid;
+            background: #ccc;
+            float: left;
+            display: inline;
+        }
+        .blue {
+            color: #fff;
+            background: blue;
+        }
+        .yellow {
+            background: yellow;
+        }
+        .pink {
+            background: pink;
+        }
+        .red {
+            color: #fff;
+            background: red;
+        }
+        #table {
+            margin-top: 20px;
+            width: 100%;
+        }
+        #header img {
+            float: right;
+        }
     </style>
 </head>
 <body>
-<?php
-    if (isset($_POST['title'])):?>
+<?php if (isset($_POST['title'])):?>
+    <div id="header">
+        <img src="https://www.html2pdf.fr/img/_langue/en/logo.gif" alt="HTML2pdf"/>
         <h1><?=$_POST['title']?></h1>
-<?php endif; ?>
+    </div>
+<?php endif;?>
 
     <h2>More information</h2>
     <p>This is my first test</p>
+
+    <div id="boxes">
+        <div class="box blue">Box 1</div>
+        <div class="box red">Box 2</div>
+        <div class="box yellow">Box 3</div>
+        <div class="box pink">Box 4</div>
+    </div>
+
+    <table border="1" id="table">
+        <tr>
+            <td>Some title 1</td>
+            <td>Some description 1</td>
+        </tr>
+        <tr>
+            <td>Some title 2</td>
+            <td>Some description 2</td>
+        </tr>
+    </table>
 </body>
 </html>
